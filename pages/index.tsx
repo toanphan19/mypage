8f89b85b1abbc4ea3a2e7093211b9708cee1b164
Toan Phan <toanphan.cs@gmail.com>
2022-05-25 18:44:28 +0200

    feat: add meme on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 
 
 const Home: NextPage = () => {
@@ -11,22 +12,28 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-start px-40 pt-10 text-center">
-        <h1 className="mb-8 text-4xl font-medium">
-          Welcome to Toan's Home Page
+
+
+      <main className="flex w-full flex-1 flex-col items-center justify-start p-4 text-center">
+        <h1 className="mb-8 text-3xl font-medium">
+          👋 Hi I'm Toan!
         </h1>
 
+        <div className="mb-8">
+          <Image src="/meme1.png" width="475" height="600" />
+        </div>
+
         <div className="flex w-full flex-col items-center">
-          <Link href="/memes">
-            <a className="my-2 w-2/3 rounded-lg border px-6 py-2 text-left hover:text-green-600 focus:text-green600">
-              <h3 className="mb-2 text-xl font-medium">Memes &rarr;</h3>
-              <p>My all-time favourite memes.</p>
+          <Link href="https://playground.toanphan.dev/wordle">
+            <a className="my-2  w-full lg:w-1/2 rounded-lg border px-4 py-2 text-left hover:text-green-600 focus:text-green600">
+              <h3 className="mb-2 text-xl font-medium">Wordle Solver &rarr;</h3>
+              <p>A statistical solver for the trending word game Wordle</p>
             </a>
           </Link>
           <Link href="/memes">
-            <a className="my-2 w-2/3 rounded-lg border px-6 py-2 text-left hover:text-green-600 focus:text-green600">
-              <h3 className="mb-2 text-xl font-medium">Wordle Solver &rarr;</h3>
-              <p>A solver for the trending word game Wordle</p>
+            <a className="my-2 w-full lg:w-1/2 rounded-lg border px-4 py-2 text-left hover:text-green-600 focus:text-green600">
+              <h3 className="mb-2 text-xl font-medium">Memes &rarr;</h3>
+              <p>My all-time favourite memes.</p>
             </a>
           </Link>
         </div>
